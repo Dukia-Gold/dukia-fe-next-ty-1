9338aca40f47885fd996ccaa6c0fd392f568e7b5
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2024-05-29 13:45:21 +0100

    Another minor test

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ const RegisterPage = () => {
   return (
     <main className="h-screen bg-[#f1f5f9] pt-24 flex flex-col justify-center items-center">
       <div className="w-[75%] h-[50%] shadow-lg">
-        <div>LoginPage</div>
+        <div>RegisterPage</div>
         <form action=""></form>
       </div>
     </main>
