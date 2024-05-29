64cde1bca93248ca71c0b9af8fa061db7c94ed18
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2024-05-29 13:03:38 +0100

    Minor test

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,11 @@
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <main className="h-screen bg-[#f1f5f9] pt-24 flex flex-col justify-center items-center">
+      <div className="w-[75%] h-[50%] shadow-lg">
+        <div>LoginPage</div>
+        <form action=""></form>
+      </div>
+    </main>
   )
 }
 
