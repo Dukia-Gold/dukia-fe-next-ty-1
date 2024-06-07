a5709a54912b0728d875fa0f177b0cc091fad81b
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-06-07 16:26:46 +0100

    Deployment error fixed(2)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,20 @@
+// import Image from "next/image";
+
 const Footer = () => {
   return (
     <footer>
+      <div>
+        {/*<Image src="" alt="" />*/}
 
+        <div></div>
+      </div>
+
+      <div>
+        <div className=""></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </footer>
   )
 }
