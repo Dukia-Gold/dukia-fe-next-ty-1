58778fccd40a2e1a69772a30dfee9f19f61f60ae
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-06-10 14:58:13 +0100

    Footer DONE

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={manrope.className}>
+      <body className={`${manrope.className} min-h-screen`}>
         <Header />
 
         {children}
