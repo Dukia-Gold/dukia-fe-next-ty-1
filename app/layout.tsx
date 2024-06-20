8efc437cc4fb9b37f39047e44e0d5074d403d108
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-06-20 12:12:31 +0100

    Favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Manrope } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/landingPageComponents/Header";
 import Footer from "@/components/landingPageComponents/Footer";
