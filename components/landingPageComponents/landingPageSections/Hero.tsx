a6450c6a1f22d76f41fa820b8731158bbc68da55
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-07-01 13:35:41 +0100

    Hero text change

--- components/landingPageComponents/landingPageSections/Hero.tsx
+++ components/landingPageComponents/landingPageSections/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="relative min-h-screen bg-hero-pattern bg-cover bg-no-repeat bg-center bg-fixed pt-48 xl:pt-36 px-3 md:px-12 xl:px-28 py-4 flex flex-col gap-10 xl:flex-row justify-center xl:justify-between items-center bg-gray-500">
       <div className="absolute top-0 left-0 h-full w-full bg-black bg-opacity-60"></div>
       <div className="z-20 flex flex-col gap-6">
-        <div className="md:w-[33.875rem] flex flex-col gap-2 text-white">
+        <div className="flex flex-col gap-2 text-white">
           {/* <p className="font-extrabold text-4xl">
             Dukia Gold is the leading full-service bullion merchant in Nigeria,
             West Africa. From refining to providing easy access to
@@ -20,14 +20,27 @@ const Hero = () => {
             you are experiencing any difficulty, please call us on +234 703 323
             8121 or +234 903 150 6699 and we will do our best to assist.
           </p> */}
-          <p className="font-extrabold text-4xl">
+
+          <h1 className="text-base sm:text-lg md:text-xl  font-semibold text-dukiaGrey">
+            BARS • COINS • POOL ALLOCATED
+          </h1>
+          <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-6xl font-bold mt-4">
+            Dukia Gold:<br/>Fortify Your Wealth
+          </h2>
+          <p className="font text-dukiaGrey text-base sm:text-lg md:text-xl lg:text-2xl mt-6 2xl:w-[70%]">
+            Buy, sell, or invest in investment-grade gold and other precious
+            metals through Nigeria&apos;s premier bullion dealer. Secure your
+            financial future with our safe and secure trading platform.
+          </p>
+
+          {/* <p className="font-extrabold text-4xl">
             Dukia Gold is the Foremost & Largest Full-Service Bullion Dealer in
             Nigeria, West Africa
           </p>
           <p>
             Creating ease of access to investment grade gold & other precious
             metals in Nigeria via a safe and secure trading platform
-          </p>
+          </p> */}
         </div>
 
         <div>
