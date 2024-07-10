a45f513b651b3e4216151675d90cc25091fdd78c
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-07-10 15:35:15 +0100

    Minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={`${manrope.className} min-h-screen`}>
         <Header />
         <main className="min-h-screen flex flex-col justify-between">
